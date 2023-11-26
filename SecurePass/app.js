b3ba612ce2a6dab099da0866b707b6da9cee2ccc
priti-jindal <128363218+priti-jindal@users.noreply.github.com>
2023-11-26 22:19:14 +0530

    Add files via upload

---
+++ SecurePass/app.js
@@ -0,0 +1,12 @@
+const start = document.querySelector("#start");
+
+start.addEventListener('click', function() {
+    // Redirect to Google's home page
+    window.location.href = "./signup.html";
+});
+
+const sp = document.querySelector("#sp");
+
+sp.addEventListener('click', function() {
+    window.location.href = "https://www.google.com";
+});
